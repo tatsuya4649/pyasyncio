ee2162ae52640908547c8faf3bbea70582ce6045
tatsuya <tatsuya4649developer@gmail.com>
2021-06-26 17:23:00 +0900

    generator

--- genea.py
+++ genea.py
@@ -17,5 +17,16 @@ for tag in range(3):
 	task = f(tag)
 	print(task)
 	tasks += [task]
+# exec in order for `tasks`
+# so ... output
+# 0
+# 1
+# 2
+# 0
+# 1
+# 2
+# 0
+# 1
+# 2
 res = loop.run_until_complete(asyncio.gather(*tasks))
 print(res)
